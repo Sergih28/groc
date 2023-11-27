7351d7c8c0904744aafc1614fa0c39dab9a2a190
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-11-27 08:48:25 +0100

    feat(story): add footer story

---
+++ app/src/stories/Footer.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Footer from '../components/Footer'
+
+const meta: Meta<typeof Footer> = {
+  component: Footer,
+}
+
+type Story = StoryObj<typeof Footer>
+
+export const NoRangeYearFooter: Story = {
+  args: {
+    currentYear: 2023,
+  },
+}
+
+export const RangeYearFooter: Story = {
+  args: {
+    currentYear: 2026,
+  },
+}
+
+export default meta
