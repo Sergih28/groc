6e364dc57e141aaa79fb189ee84366584fdc6f03
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-11-28 07:42:55 +0100

    feat(story): add next button story

---
+++ app/src/stories/NextButton.stories.tsx
@@ -0,0 +1,16 @@
+import NextButton from '../components/NextButton'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof NextButton> = {
+  component: NextButton,
+}
+
+type Story = StoryObj<typeof NextButton>
+
+export const Next: Story = {
+  args: {
+    nextPomodoro: () => console.log('next pomodoro'),
+  },
+}
+
+export default meta
