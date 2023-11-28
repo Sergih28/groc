04f9097cb7d21dd7c82d26e71e8dc579a912b9ff
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-11-28 08:17:27 +0100

    feat(story): add slider story

---
+++ app/src/stories/Slider.stories.tsx
@@ -0,0 +1,20 @@
+import Slider from '../components/Slider'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof Slider> = {
+  component: Slider,
+  title: 'Slider Stories',
+}
+type Story = StoryObj<typeof Slider>
+
+export const DefaultSlider: Story = {}
+
+export const CustomSlider: Story = {
+  args: {
+    initialValue: 20,
+    maxValue: 360,
+    minValue: 10,
+  },
+}
+
+export default meta
