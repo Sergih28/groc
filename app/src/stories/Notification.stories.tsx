9b76b3575df6c8986c71077758188617900173a7
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-12-07 10:52:46 +0100

    feat(story): add notification story

---
+++ app/src/stories/Notification.stories.tsx
@@ -0,0 +1,74 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Notification from '../components/Notification/index'
+
+const meta: Meta<typeof Notification> = {
+  component: Notification,
+}
+
+export default meta
+type Story = StoryObj<typeof Notification>
+
+export const InfoNotification: Story = {
+  args: {
+    customization: {
+      position: 'top-center',
+      expand: true,
+      richColors: true,
+      theme: 'light',
+      visibleToasts: 3,
+      closeButton: true,
+      offset: 32,
+    },
+  },
+}
+
+export const SuccessNotification: Story = {
+  args: {
+    customization: {
+      position: 'top-right',
+      expand: false,
+      theme: 'dark',
+      visibleToasts: 6,
+      closeButton: true,
+    },
+  },
+}
+
+export const ErrorNotification: Story = {
+  args: {
+    customization: {
+      position: 'top-left',
+      expand: true,
+      richColors: true,
+      theme: 'light',
+      visibleToasts: 2,
+      closeButton: true,
+    },
+  },
+}
+
+export const MessageNotification: Story = {
+  args: {
+    customization: {
+      position: 'bottom-center',
+      expand: true,
+      richColors: true,
+      theme: 'light',
+      visibleToasts: 1,
+      closeButton: true,
+    },
+  },
+}
+
+export const LoadingNotification: Story = {
+  args: {
+    customization: {
+      position: 'top-center',
+      expand: true,
+      richColors: true,
+      theme: 'dark',
+      visibleToasts: 2,
+      closeButton: true,
+    },
+  },
+}
