fd173c9dc169b7640d7ef674106717a9552ca28f
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-12-11 07:28:30 +0100

    build(test): add test for NextButton component

---
+++ app/tests/unit/NextButton.test.tsx
@@ -0,0 +1,33 @@
+/// <reference lib="dom" />
+import NextButton from '@components/NextButton'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
+
+describe('NextButton component test', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  test('renders the next button', () => {
+    render(<NextButton nextPomodoro={() => {}} />)
+  })
+
+  test('renders the next button with the "Next" text', () => {
+    render(<NextButton nextPomodoro={() => {}} />)
+
+    const nextButtonElement = screen.getByText(/Next/i)
+
+    expect(nextButtonElement).toBeInTheDocument()
+  })
+
+  test('next button function is called', () => {
+    const mockFunction = vi.fn()
+
+    render(<NextButton nextPomodoro={mockFunction} />)
+
+    const nextButtonElement = screen.getByRole('button')
+
+    fireEvent.click(nextButtonElement)
+
+    expect(mockFunction).toHaveBeenCalled()
+  })
+})
