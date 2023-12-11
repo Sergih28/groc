704ec194b4c1122bf93bc342ec82ea711b7de0a2
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-12-11 07:24:10 +0100

    build(test): add test for ResetButton component

---
+++ app/tests/unit/ResetButton.test.tsx
@@ -0,0 +1,26 @@
+/// <reference lib="dom" />
+import ResetButton from '@components/ResetButton'
+import { render, screen, cleanup, fireEvent } from '@testing-library/react'
+
+describe('ResetButton component test', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  test('renders the reset button', () => {
+    render(<ResetButton resetPomodoro={() => {}} />)
+  })
+
+  test('reset button function is called', () => {
+    const mockFunction = vi.fn()
+
+    render(<ResetButton resetPomodoro={mockFunction} />)
+
+    const resetButtonElement = screen.getByRole('button')
+
+    fireEvent.click(resetButtonElement)
+
+    expect(mockFunction).toHaveBeenCalled()
+    expect(mockFunction).toHaveBeenCalledTimes(1)
+  })
+})
