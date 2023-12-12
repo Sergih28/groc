fd013087ef8ce0ec39bceeb287a1043b29da2c32
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-12-12 08:20:31 +0100

    build(vite.config.ts): add vite.config.ts

---
+++ app/vite.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+  },
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+    },
+  },
+})
