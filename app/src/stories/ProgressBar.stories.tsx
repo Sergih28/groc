c104e885d72e0f7e45a67639657bfc5ca7329d46
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-12-13 08:22:59 +0100

    feat(story): add ProgressBar story

---
+++ app/src/stories/ProgressBar.stories.tsx
@@ -0,0 +1,33 @@
+import ProgressBar from '../components/ProgressBar'
+import type { Meta, StoryObj } from '@storybook/react'
+import '../styles/index.css'
+
+const meta: Meta<typeof ProgressBar> = {
+  component: ProgressBar,
+}
+
+type Story = StoryObj<typeof ProgressBar>
+
+export const ProgressBarWithPercentage: Story = {
+  args: {
+    baseValue: 0,
+    currentValue: 100,
+    targetValue: 100,
+    showPercentage: true,
+    backgroundColor: 'yellow',
+    fillColor: 'blue',
+  },
+}
+
+export const ProgressBarNoPercentage: Story = {
+  args: {
+    baseValue: 0,
+    currentValue: 45,
+    targetValue: 100,
+    showPercentage: false,
+    backgroundColor: 'red',
+    fillColor: 'purple',
+  },
+}
+
+export default meta
