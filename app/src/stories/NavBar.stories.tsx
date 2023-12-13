69a4e34eca5ea465cc2da1fcf2a5aed12c619975
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-12-13 08:34:28 +0100

    feat(story): add NavBar story

---
+++ app/src/stories/NavBar.stories.tsx
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof NavBar> = {
+  component: NavBar,
+}
+
+type Story = StoryObj<typeof NavBar>
+
+export const MainNavBar: Story = {}
+
+export default meta
