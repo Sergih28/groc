d6f62fb600cc1db76aaee5e091bfe1ffca589eb1
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-12-15 09:24:27 +0100

    feat(story): add Settings story

---
+++ app/src/stories/Settings.stories.tsx
@@ -0,0 +1,12 @@
+import Settings from '../components/Settings'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<typeof Settings> = {
+  component: Settings,
+}
+
+type Story = StoryObj<typeof Settings>
+
+export const DefaultSettings: Story = {}
+
+export default meta
