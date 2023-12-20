a603b0bc1cbafed350e8f47d06c990d5a4608f10
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2023-12-20 12:31:06 +0100

    feat(translation): add translations for Footer component

--- app/src/i18n/locales/en/_export.ts
+++ app/src/i18n/locales/en/_export.ts
@@ -1,5 +1,6 @@
 import common from './common.json'
 import nav from './nav.json'
 import settings from './settings.json'
+import footer from './footer.json'
 
-export default { ...common, ...nav, ...settings }
+export default { ...common, ...nav, ...settings, ...footer }
