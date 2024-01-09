97dc2795f2e81dd81a8d6e917b588440544b592e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-09 14:00:49 +0100

    feat(test): add unit test for Notification component

---
+++ app/tests/unit/Notification.test.tsx
@@ -0,0 +1,35 @@
+import Notification from '@components/Notification/'
+import Button from '@components/Button'
+import { toast } from 'sonner'
+import { screen, render, fireEvent, cleanup } from '@testing-library/react'
+
+const NotificationWrapper = ({ message = 'Notification message' }: { message?: string }) => {
+  return (
+    <>
+      <Notification />
+      <Button handleClick={() => toast(message)}>Click to notify</Button>
+    </>
+  )
+}
+
+describe('Notification component test', () => {
+  afterEach(() => {
+    cleanup()
+  })
+
+  test('render the Notification component', () => {
+    render(<NotificationWrapper />)
+  })
+
+  test('given a certain message and firing the click event, render the notification with that message', async () => {
+    const message = 'Checking the notification component'
+
+    render(<NotificationWrapper message={message} />)
+
+    const buttonElement = screen.getByRole('button')
+
+    fireEvent.click(buttonElement)
+
+    expect(await screen.findByText(message)).toBeInTheDocument()
+  })
+})
