22ad2ba95f6aa03c2236ca6a110b8ec93e04e3d9
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-17 07:38:52 +0100

    fix(test): update calculateCounter function to test its new functionality

--- app/tests/unit/calculateCounter.test.ts
+++ app/tests/unit/calculateCounter.test.ts
@@ -1,67 +1,17 @@
 import { calculateCounter } from '@hooks/useCounter'
 
 describe('calculateCounter()', () => {
-  describe('Incrementing scenarios', () => {
-    describe('counter does not reach the target (seconds)', () => {
-      test.each([
-        [1, 10, true, 1, 2],
-        [23, 120, true, 2, 25],
-        [200, 400, true, 100, 300],
-      ])(
-        'counter %i and target (seconds) %i, increments being %o by %i returns %i',
-        (prevCounter, seconds, isIncrementing, countingInterval, expected) => {
-          const result = calculateCounter(prevCounter, seconds, isIncrementing, countingInterval)
+  test.each([
+    [1, 1, 2],
+    [23, 2, 25],
+    [120, 100, 220],
+    [2000, 1000, 3000],
+  ])(
+    'counter %i and target (seconds) %i, increments being %o by %i returns %i',
+    (prevCounter, countingInterval, expected) => {
+      const result = calculateCounter(prevCounter, countingInterval)
 
-          expect(result).toBe(expected)
-        },
-      )
-    })
-
-    describe('counter reaches the target (seconds)', () => {
-      test.each([
-        [9, 10, true, 1, 10],
-        [121, 120, true, 10, 120],
-        [401, 400, true, 100, 400],
-      ])(
-        'counter %i and target (seconds) %i, increments being %o by %i returns target (%i)',
-        (prevCounter, seconds, isIncrementing, countingInterval, expected) => {
-          const result = calculateCounter(prevCounter, seconds, isIncrementing, countingInterval)
-
-          expect(result).toBe(expected)
-        },
-      )
-    })
-  })
-
-  describe('Decrementing scenarios', () => {
-    describe('counter does not reach the target (seconds)', () => {
-      test.each([
-        [2, 10, false, 1, 1],
-        [120, 300, false, 10, 110],
-        [45, 60, false, 10, 35],
-      ])(
-        'counter %i and target (seconds) %i, decrements being %o by %i returns %i',
-        (prevCounter, seconds, isIncrementing, countingInterval, expected) => {
-          const result = calculateCounter(prevCounter, seconds, isIncrementing, countingInterval)
-
-          expect(result).toBe(expected)
-        },
-      )
-    })
-
-    describe('counter reaches the target (seconds)', () => {
-      test.each([
-        [0, 10, false, 1, 0],
-        [-12, 400, false, 25, 0],
-        [0, 400, false, 30, 0],
-      ])(
-        'counter %i and target (seconds) %i, decrements being %o by %i returns target (%i)',
-        (prevCounter, seconds, isIncrementing, countingInterval, expected) => {
-          const result = calculateCounter(prevCounter, seconds, isIncrementing, countingInterval)
-
-          expect(result).toBe(expected)
-        },
-      )
-    })
-  })
+      expect(result).toBe(expected)
+    },
+  )
 })
