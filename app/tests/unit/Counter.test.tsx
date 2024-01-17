cfa9e42b5c736368d3772adcb0cc28af741ca60f
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-17 07:38:24 +0100

    feat(test): add unit test for calculateElapsedTime and fix test

--- app/tests/unit/Counter.test.tsx
+++ app/tests/unit/Counter.test.tsx
@@ -1,22 +1,33 @@
-import Counter from '@molecules/Counter'
-import { $settings } from '@store/Pomodoro'
 import { act, cleanup, fireEvent, render, screen } from '@testing-library/react'
 
-import { resetSettingsState } from './helpers'
+import { resetGlobalState } from './helpers'
+import { calculateElapsedTime } from '@components/ui/molecules/Counter/functions'
+import useCounter from '@hooks/useCounter'
+import Counter from '@molecules/Counter'
+import { updateSettings } from '@store/Pomodoro'
+
+const CounterWrapper = () => {
+  useCounter()
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
 
 describe('Counter component test', () => {
-  afterEach(() => {
+  beforeEach(() => {
     cleanup()
-    resetSettingsState()
+    resetGlobalState()
   })
 
   test('render Counter component', () => {
-    render(<Counter />)
+    render(<CounterWrapper />)
   })
 
   describe('Counter format renders', () => {
     test('given counter with minutes format, renders the time left in minutes format', () => {
-      render(<Counter />)
+      render(<CounterWrapper />)
 
       const counterContentElement = screen.getByText('25:00')
 
@@ -24,9 +35,8 @@ describe('Counter component test', () => {
     })
 
     test('given counter with seconds format, renders the time left in seconds format', () => {
-      $settings.set({ ...$settings.get(), counterFormat: 'seconds' })
-
-      render(<Counter />)
+      updateSettings({ counterFormat: 'seconds' })
+      render(<CounterWrapper />)
 
       const counterContentElement = screen.getByText('1500')
       expect(counterContentElement).toBeInTheDocument()
@@ -38,11 +48,11 @@ describe('Counter component test', () => {
       vi.useFakeTimers()
     })
     afterEach(() => {
-      vi.restoreAllMocks()
+      vi.useRealTimers()
     })
 
     test('counter updates value after a few seconds', async () => {
-      render(<Counter />)
+      render(<CounterWrapper />)
 
       const buttonContinue = screen.getByRole('button', { name: 'Continue' })
       fireEvent.click(buttonContinue)
@@ -51,24 +61,76 @@ describe('Counter component test', () => {
         vi.advanceTimersByTime(5000)
       })
 
-      expect(screen.getByText('24:55'))
+      expect(screen.findByText('24:55'))
     })
 
     test('given a counter value that has reached the target, the counter remains at the target value after additional time passes', () => {
-      $settings.set({ ...$settings.get(), pomodoroDuration: 60 })
+      render(<CounterWrapper />)
 
-      render(<Counter />)
-
-      expect(screen.getByText('01:00'))
+      expect(screen.getByText('25:00'))
 
       const buttonContinue = screen.getByRole('button', { name: 'Continue' })
       fireEvent.click(buttonContinue)
 
       act(() => {
-        vi.advanceTimersByTime(70 * 1000)
+        vi.advanceTimersByTime(25 * 60 * 1000)
       })
 
-      expect(screen.getByText('00:00'))
+      expect(screen.findByText('00:00'))
+    })
+  })
+  describe('calculatElapsedTime', () => {
+    beforeAll(() => {
+      const unixTime = 1689941745
+      Date.now = vi.fn(() => unixTime)
+    })
+
+    afterAll(() => {
+      vi.resetAllMocks()
+    })
+
+    const ONE_MINUTE_MS = 60 * 1000
+    test('given a started time being now with no pauses or end time, the elapsed time should be 0', () => {
+      const elapsedTime = calculateElapsedTime(Date.now(), [])
+
+      expect(elapsedTime).toBe(0)
+    })
+
+    test('given a start time with no time range that has surpassed the expected duration, should return 0', () => {
+      const START_TIME = Date.now() - ONE_MINUTE_MS * 30
+      const elapsedTime = calculateElapsedTime(START_TIME, [])
+
+      expect(elapsedTime).toBe(1800)
+    })
+
+    test('given a start time and a paused time range, should return the remaining time', () => {
+      const START_TIME = Date.now() - ONE_MINUTE_MS * 10
+      const pausedTimes = [
+        {
+          start: START_TIME + ONE_MINUTE_MS * 5,
+          end: START_TIME + ONE_MINUTE_MS * 10,
+        },
+      ]
+      const elapsedTime = calculateElapsedTime(START_TIME, pausedTimes)
+
+      expect(elapsedTime).toBe(300)
+    })
+
+    test('given a start time and a paused time range that has not fishined, should return the remainin time', () => {
+      const START_TIME = Date.now() - ONE_MINUTE_MS * 60
+      const pausedTimes = [
+        {
+          start: START_TIME + ONE_MINUTE_MS * 30,
+          end: START_TIME + ONE_MINUTE_MS * 35,
+        },
+        {
+          start: START_TIME + ONE_MINUTE_MS * 55,
+          end: null,
+        },
+      ]
+      const elapsedTime = calculateElapsedTime(START_TIME, pausedTimes)
+
+      expect(elapsedTime).toBe(3000)
     })
   })
 })
