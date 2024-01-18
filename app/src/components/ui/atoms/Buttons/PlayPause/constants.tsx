c1e59b89ad9819f8fb9f1a0e17d625844ef62c3e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-18 11:14:09 +0100

    feat(ui): add testId prop to Button, refactor components to accommodate that change

--- app/src/components/ui/atoms/Buttons/PlayPause/constants.tsx
+++ app/src/components/ui/atoms/Buttons/PlayPause/constants.tsx
@@ -1,1 +1,8 @@
-export const DEFAULT_TEXT = 'Play'
+import TEST_ID from '@data/testIds'
+
+const DEFAULT_VALUES = {
+  text: 'Play',
+  testId: TEST_ID.pomodoro.continueButton,
+}
+
+export default DEFAULT_VALUES
