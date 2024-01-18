413158b56e1918772582f307496fd3e1685a4516
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-18 11:14:09 +0100

    feat(ui): add testId prop to Button, refactor components to accommodate that change

--- app/src/components/ui/molecules/Counter/index.tsx
+++ app/src/components/ui/molecules/Counter/index.tsx
@@ -1,10 +1,11 @@
-import PlayPauseButton from '@atoms/Buttons/PlayPause'
-import ResetButton from '@atoms/Buttons/Reset'
 import { useStore } from '@nanostores/react'
-import { $counter } from '@store/Pomodoro'
 
 import { ContentWrapper } from './components'
 import { BUTTON_TEXT } from './constants'
+import PlayPauseButton from '@atoms/Buttons/PlayPause'
+import ResetButton from '@atoms/Buttons/Reset'
+import TEST_ID from '@data/testIds'
+import { $counter } from '@store/Pomodoro'
 
 const Counter = () => {
   const { counterContent, isPaused, handleReset, handlePause, counterValue } = useStore($counter)
@@ -14,13 +15,19 @@ const Counter = () => {
     : isPaused
       ? BUTTON_TEXT.CONTINUE
       : BUTTON_TEXT.PAUSE
-
+  const buttonTestId = !hasStarted
+    ? TEST_ID.pomodoro.startButton
+    : isPaused
+      ? TEST_ID.pomodoro.continueButton
+      : TEST_ID.pomodoro.pauseButton
   return (
     <>
       <ContentWrapper>
-        <span className="text-5xl font-semibold text-white">{counterContent}</span>
+        <span data-testid="counter-content" className="text-5xl font-semibold text-white">
+          {counterContent}
+        </span>
         <div className="flex gap-4">
-          <PlayPauseButton text={buttonText} handleClick={handlePause} />
+          <PlayPauseButton text={buttonText} handleClick={handlePause} testId={buttonTestId} />
           <ResetButton resetPomodoro={handleReset} />
         </div>
       </ContentWrapper>
