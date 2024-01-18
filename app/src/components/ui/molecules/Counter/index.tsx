387646e606b2da6974bfa956d48943ea2c387276
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-18 07:23:50 +0100

    feat(molecule): update Counter after PlayPauseButton changes, add button text constant

--- app/src/components/ui/molecules/Counter/index.tsx
+++ app/src/components/ui/molecules/Counter/index.tsx
@@ -1,19 +1,26 @@
-import { useStore } from '@nanostores/react'
-
-import { ContentWrapper } from './components'
 import PlayPauseButton from '@atoms/Buttons/PlayPause'
 import ResetButton from '@atoms/Buttons/Reset'
+import { useStore } from '@nanostores/react'
 import { $counter } from '@store/Pomodoro'
 
+import { ContentWrapper } from './components'
+import { BUTTON_TEXT } from './constants'
+
 const Counter = () => {
-  const { counterContent, isPaused, handleReset, handlePause } = useStore($counter)
+  const { counterContent, isPaused, handleReset, handlePause, counterValue } = useStore($counter)
+  const hasStarted = counterValue > 0
+  const buttonText = !hasStarted
+    ? BUTTON_TEXT.START
+    : isPaused
+      ? BUTTON_TEXT.CONTINUE
+      : BUTTON_TEXT.PAUSE
 
   return (
     <>
       <ContentWrapper>
         <span className="text-5xl font-semibold text-white">{counterContent}</span>
         <div className="flex gap-4">
-          <PlayPauseButton isPaused={isPaused} handleClick={handlePause} />
+          <PlayPauseButton text={buttonText} handleClick={handlePause} />
           <ResetButton resetPomodoro={handleReset} />
         </div>
       </ContentWrapper>
