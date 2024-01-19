4c26821336348f98e7d30522371232730c6c735e
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-19 13:04:45 +0100

    fix(phase): fix phase change by adding handlePhase function and updating components and hooks

--- app/src/components/ui/organisms/Pomodoro/index.tsx
+++ app/src/components/ui/organisms/Pomodoro/index.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react'
 
+import { useStore } from '@nanostores/react'
+
 import ProgressBar from '@atoms/ProgressBar'
 import Button from '@components/ui/atoms/Buttons/Button'
 import { loadActivePomodoro } from '@components/ui/molecules/Counter/functions'
 import useCounter from '@hooks/useCounter'
 import Counter from '@molecules/Counter'
-import { updatePhase } from '@store/Pomodoro'
 import { $counter } from '@store/Pomodoro'
 
 const Pomodoro = () => {
@@ -15,22 +16,23 @@ const Pomodoro = () => {
     loadActivePomodoro()
   }, [])
 
+  const { handlePhase } = useStore($counter)
+
   return (
     <>
       <ProgressBar />
       <div className="mb-6 flex justify-center gap-2 align-middle">
-        <Button handleClick={() => updatePhase('pomodoro')} styles="options">
+        <Button handleClick={() => handlePhase('pomodoro')} styles="options">
           Pomodoro
         </Button>
-        <Button handleClick={() => updatePhase('break')} styles="options">
+        <Button handleClick={() => handlePhase('break')} styles="options">
           Break
         </Button>
-        <Button handleClick={() => updatePhase('longBreak')} styles="options">
+        <Button handleClick={() => handlePhase('longBreak')} styles="options">
           Long Break
         </Button>
       </div>
       <Counter />
-      {JSON.stringify($counter.get())}
     </>
   )
 }
