846c284c448da40d83d6aba2866bdf8e55d561bb
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-22 08:55:36 +0100

    feat(test): add calculateSecondsFromMinuts unit test

--- app/tests/unit/numbers.test.ts
+++ app/tests/unit/numbers.test.ts
@@ -1,4 +1,4 @@
-import { calculateSecondsFromMilliseconds } from '@utils/numbers'
+import { calculateSecondsFromMilliseconds, calculateSecondsFromMinutes } from '@utils/numbers'
 
 describe('calculateSecondsFromMilliseconds()', () => {
   test.each([
@@ -9,6 +9,20 @@ describe('calculateSecondsFromMilliseconds()', () => {
     [-2345236724356662, -2345236724356.662],
   ])('given  %d milliseconds returns %d seconds', (milliseconds, expected) => {
     const seconds = calculateSecondsFromMilliseconds(milliseconds)
+
+    expect(seconds).toBe(expected)
+  })
+})
+
+describe('calculateSecondsFromMinutes()', () => {
+  test.each([
+    [1, 60],
+    [2, 120],
+    [0, 0],
+    [-60, -3600],
+    [-234562, -14073720],
+  ])('given  %d minutes returns %d seconds', (minutes, expected) => {
+    const seconds = calculateSecondsFromMinutes(minutes)
 
     expect(seconds).toBe(expected)
   })
