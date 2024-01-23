444a367d46a2a09cbaa15b745e9c484224744903
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-23 20:19:26 +0100

    feat(astro.config): add config to use node in astro

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+
 import { fallbackLang, languages } from './src/i18n/ui'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [
     react(),
     tailwind({
