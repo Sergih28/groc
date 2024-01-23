5e2987887df06f47670e07d05cbda30f38c06587
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-23 10:35:12 +0100

    fix(i18n): merge cat with ca folder

--- app/src/i18n/locales/cat/_export.ts
+++
@@ -1,6 +0,0 @@
-import common from './common.json'
-import footer from './footer.json'
-import nav from './nav.json'
-import settings from './settings.json'
-
-export default { ...common, ...nav, ...settings, ...footer }
