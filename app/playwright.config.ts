853e2f9f716b25b20196c3d2b061c4f5209e6c08
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-26 10:26:26 +0100

    fix(playwright.config): update baseUrl for docker container, add webkit, remove mobile chrome

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -20,11 +20,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:4321',
+    baseURL: 'http://groc-dev:4321',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -42,16 +42,16 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
     /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
@@ -69,9 +69,9 @@ export default defineConfig({
   ],
 
   // Run your local dev server before starting the tests */
-  webServer: {
-    command: 'pnpm run build && pnpm run preview',
-    url: 'http://127.0.0.1:4321',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'pnpm run build && pnpm run preview',
+  //   url: 'http://127.0.0.1:4321',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 })
