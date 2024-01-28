fffb7315a02658bde87055bc9cfd9311813598c6
Sergi <Sergih28@users.noreply.github.com>
2024-01-28 10:09:31 +0000

    fix(playwright): fix how we use the base url on the tests

--- app/tests/e2e/NavBar.spec.ts
+++ app/tests/e2e/NavBar.spec.ts
@@ -1,23 +1,19 @@
 import { expect, test } from '@playwright/test'
 
-import { BASE_URL } from './utils'
-
-const URL = `${BASE_URL}/`
-
 test('has title', async ({ page }) => {
-  await page.goto(URL)
+  await page.goto('/')
 
   await expect(page).toHaveTitle(/Groc/)
 })
 
 test.describe('navbar navigation', () => {
   test('clicking the Home navbar element redirects to "/"', async ({ page }) => {
-    await page.goto(URL)
+    await page.goto('/')
 
     const homeLink = page.getByRole('link', { name: 'Home' })
 
     await homeLink.click()
 
-    await expect(page).toHaveURL(URL)
+    await expect(page).toHaveURL('/en/')
   })
 })
