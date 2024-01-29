dc3820b62ff2a9697c4fbbad6d6ccb5dfa4df16b
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-01-29 08:02:53 +0100

    feat(store): change store objects from "atom" to "map"

--- app/src/store/Pomodoro.ts
+++ app/src/store/Pomodoro.ts
@@ -1,12 +1,12 @@
 import type { UUID } from 'crypto'
 
-import { atom } from 'nanostores'
+import { atom, map } from 'nanostores'
 
 import type { CounterType, PhaseType, SettingsType } from './types'
 
 import { DEFAULT_COUNTER_VALUES, DEFAULT_SETTINGS_VALUES } from './constants'
 
-export const $counter = atom<CounterType>(DEFAULT_COUNTER_VALUES)
+export const $counter = map<CounterType>(DEFAULT_COUNTER_VALUES)
 
 export const setCounterValue = (newCounterValue: number) => {
   $counter.set({ ...$counter.get(), counterValue: newCounterValue })
@@ -16,7 +16,7 @@ export const updateCounter = (updatedCounter: Partial<CounterType>) => {
   $counter.set({ ...$counter.get(), ...updatedCounter })
 }
 
-export const $settings = atom<SettingsType>(DEFAULT_SETTINGS_VALUES)
+export const $settings = map<SettingsType>(DEFAULT_SETTINGS_VALUES)
 
 export const updateSettings = (updatedSettings: Partial<SettingsType>) => {
   $settings.set({ ...$settings.get(), ...updatedSettings })
