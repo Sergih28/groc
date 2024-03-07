5ea3ab52124fcdc4703e9b3ae777d6c516678001
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-03-07 07:28:02 +0100

    feat(hook): add use-get-animated-counter hook and utilize it in counter component

---
+++ app/src/features/pomodoro/hooks/use-get-animated-counter-content.tsx
@@ -0,0 +1,33 @@
+import { animated, SpringValue, useTransition } from '@react-spring/web'
+
+const useGetAnimatedCounterContent = (counterContent: string) => {
+  const TRANSITION = {
+    from: { opacity: 0, transform: 'translate3d(0, -80%, 0)' },
+    enter: { opacity: 1, transform: 'translate3d(0, 0%, 0)' },
+    exitBeforeEnter: true,
+    leave: { opacity: 0, transform: 'translate3d(0, 50%, 0)' },
+    initial: { opacity: 1, transform: 'translate3d(0, 0%, 0)' },
+  }
+
+  const renderAnimatedElement = (
+    style: { opacity: SpringValue<number>; transform: SpringValue<string> },
+    digit: string,
+  ) => {
+    return <animated.span style={style}>{digit}</animated.span>
+  }
+
+  const counterDigits = counterContent.split('')
+
+  const animatedCounter = () => {
+    return (
+      <>
+        {counterDigits.map((digit: string) => {
+          return useTransition(digit, TRANSITION)(renderAnimatedElement)
+        })}
+      </>
+    )
+  }
+  return { animatedCounter }
+}
+
+export default useGetAnimatedCounterContent
