0b3dac5ab2a56f783f8690da904926a5ae0c1b58
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-04-03 14:59:43 +0200

    feat(hook): implement finish sound effect and handleFinish action when the pomodoro is finished

--- app/src/features/pomodoro/hooks/use-counter.tsx
+++ app/src/features/pomodoro/hooks/use-counter.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
 
 import { useStore } from '@nanostores/react'
+// @ts-expect-error There are no types for this dependency
+import useSound from 'use-sound'
 
 import {
   loadActivePomodoro,
@@ -10,11 +12,19 @@ import {
 import { accurateTimer } from '@utils/time'
 
 import { pomodoroStore } from '@store/pomodoro'
+import { handleFinish } from '@store/pomodoro/actions'
 import { isPomodoroFinished } from '@store/pomodoro/utils'
+
+// Sound effect sourced from: https://pixabay.com/sound-effects/success-1-6297/
+import finishSound from '@assets/finish.mp3'
 
 const REFRESH_INTERVAL_MS = 100
 
 const useCounter = () => {
+  const { sound } = useStore(pomodoroStore.state)
+
+  const [playFinishSound] = useSound(finishSound, { volume: sound ? 0.5 : 0 })
+
   const { counterValue, isPaused, phase } = useStore(pomodoroStore.state)
 
   useEffect(function onLoad() {
@@ -25,6 +35,11 @@ const useCounter = () => {
     function updateCounter() {
       const timer = accurateTimer(() => {
         const isFinished = isPomodoroFinished()
+
+        if (!isPaused && isFinished) {
+          playFinishSound()
+          handleFinish()
+        }
 
         if (isPaused || isFinished) return
 
